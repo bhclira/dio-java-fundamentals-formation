80585ded7a5b6dcf2efa6cde9cd57a7d3d87b6b6
bhclira <brenohencastro@gmail.com>
2025-03-24 14:55:44 -0300

    aula - adicionar exemplos de uso do laço de repetição For em Java

--- estruturas-controle/src/For.java
+++ estruturas-controle/src/For.java
@@ -1,6 +1,24 @@
 public class For {
     public static void main(String[] args) {
-        
+        // estrutura for - laço de repetição
+        var scanner = new java.util.Scanner(System.in);
+
+        for (;;) {
+            System.out.println("Digite um nome ou exit pra sair:  ");
+            var name = scanner.next();
+
+            if(name.equalsIgnoreCase("exit")) break;
+
+            System.out.println(name);
+        }
+
+        // for com contador
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+        System.out.println(i);
+        }
     }
 
 }
