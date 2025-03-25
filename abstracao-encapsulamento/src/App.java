8ce9412b5ab360bd0664b8ae80c0da36e0688c30
bhclira <brenohencastro@gmail.com>
2025-03-25 12:12:52 -0300

    aula - adicionar estrutura inicial do projeto com classes App e Person, e configurações do VS Code

---
+++ abstracao-encapsulamento/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // classes, objetos, etc
+        var male = new Person("João", 20);
+        
+        // male.setName("João");
+        // male.setAge(20);
+
+        var female = new Person("Maria", 18);
+        
+        // female.setName("Maria");
+        // female.setAge (18);
+
+        System.out.println("Male name: " + male.getName() + "\n age: " + male.getAge());
+        System.out.println("female name: " + female.getName() + "\n age: " + female.getAge());
+
+
+    }
+}
---
+++ abstracao-encapsulamento/src/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    private final String name;
+    private int age;
+
+    // construtor com parâmetros
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
